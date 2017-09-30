0d8a8aa4e7b6d1e5d99e6d6eefc16b39d681f54d
anasmatic <anasmatic@gmail.com>
2017-10-01 01:20:07 +0800

    + Laser Logic
    missing files which I wrote Laser logic code in, are now attached

---
+++ term2/CarND-Project1-Extended-Kalman-Filter/src/tools.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include "tools.h"
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+  TODO:
+    * Calculate the RMSE here.
+  */
+	VectorXd rmse(4);
+	rmse << 0, 0, 0, 0;
+
+	// check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	//  * the estimation vector size should equal ground truth vector size
+	if (estimations.size() != ground_truth.size()
+		|| estimations.size() == 0) {
+		cout << "Invalid estimation or ground_truth data" << endl;
+		return rmse;
+	}
+
+	//accumulate squared residuals
+	for (unsigned int i = 0; i < estimations.size(); ++i) {
+
+		VectorXd residual = estimations[i] - ground_truth[i];
+
+		//coefficient-wise multiplication
+		residual = residual.array()*residual.array();
+		rmse += residual;
+	}
+
+	//calculate the mean
+	rmse = rmse / estimations.size();
+
+	//calculate the squared root
+	rmse = rmse.array().sqrt();
+
+	//return the result
+	return rmse;
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+  /**
+  TODO:
+    * Calculate a Jacobian here.
+  */
+	MatrixXd Hj(3, 4);
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	//pre-compute a set of terms to avoid repeated calculation
+	float c1 = px*px + py*py;
+	float c2 = sqrt(c1);
+	float c3 = (c1*c2);
+
+	//check division by zero
+	if (fabs(c1) < 0.0001) {
+		cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+		return Hj;
+	}
+
+	//compute the Jacobian matrix
+	Hj << (px / c2), (py / c2), 0, 0,
+		-(py / c1), (px / c1), 0, 0,
+		py*(vx*py - vy*px) / c3, px*(px*vy - py*vx) / c3, px / c2, py / c2;
+
+	return Hj;
+}
+
+/**
+* Convert radar from polar to cartesian coordinates
+*/
+VectorXd Tools::ConvertPolarToCartesian(VectorXd x_measurements) {
+	float ro = x_measurements[0];//rho , range
+	float theta = x_measurements[1];//phi , bearing
+	float ro_dot = x_measurements[2];//rho , radial velocity
+	VectorXd converted_raw_measurements_ = VectorXd(2);
+	float px = ro * cos(theta);
+	float py = ro * sin(theta);
+	converted_raw_measurements_ << px, py,0,0;
+	return converted_raw_measurements_;
+}
